8c77952cba4fd3a1ba2225a83c1a4eecc65de8e5
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-06-20 07:17:18 +0200

    Add Callback Response in Temporal

---
+++ src/Job/Application/Service/CompanyService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Job\Application\Service;
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class CompanyService
+{
+
+    public function __construct(
+        private string $logoCompanyDirectory,
+        private SluggerInterface $slugger
+    ) {
+    }
+    /**
+     * @param Request $request
+     *
+     * @return string|JsonResponse
+     */
+    public function uploadLogo(Request $request): string|JsonResponse
+    {
+        $file = $request->files->get('file');
+
+        if (!$file) {
+            return new JsonResponse(['error' => 'No file uploaded.'], 400);
+        }
+
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+        $newFilename = $safeFilename.'-'.uniqid('', true).'.'.$file->guessExtension();
+
+        try {
+            $file->move(
+                $this->logoCompanyDirectory,
+                $newFilename
+            );
+        } catch (FileException $e) {
+            return new JsonResponse(['error' => $e->getMessage()], 500);
+        }
+        $baseUrl = $request->getSchemeAndHttpHost();
+        $relativePath = '/uploads/companies/logo/' . $newFilename;
+
+        return $baseUrl . $relativePath;
+    }
+}
