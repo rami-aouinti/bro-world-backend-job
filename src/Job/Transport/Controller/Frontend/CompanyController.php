14e5ec4328c430b4da2336ef05854171b0d7edbb
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-08-09 19:35:36 +0200

    add Cv Templates

--- src/Job/Transport/Controller/Frontend/CompanyController.php
+++ src/Job/Transport/Controller/Frontend/CompanyController.php
@@ -4,21 +4,16 @@ declare(strict_types=1);
 
 namespace App\Job\Transport\Controller\Frontend;
 
-use App\General\Domain\Utils\JSON;
 use App\Job\Application\ApiProxy\UserProxy;
 use App\Job\Infrastructure\Repository\CompanyRepository;
-use JsonException;
 use OpenApi\Attributes as OA;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 /**
  * @package App\Company
@@ -28,9 +23,9 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 readonly class CompanyController
 {
     public function __construct(
-        private SerializerInterface $serializer,
         private CompanyRepository   $companyRepository,
-        private UserProxy           $userProxy
+        private UserProxy           $userProxy,
+        private TagAwareCacheInterface $cache
     ) {
     }
 
@@ -39,12 +34,7 @@ readonly class CompanyController
      *
      * @param Request $request
      * @return JsonResponse
-     * @throws JsonException
-     * @throws ClientExceptionInterface
-     * @throws DecodingExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
+     * @throws InvalidArgumentException
      */
     #[Route(
         path: '/platform/company',
@@ -52,33 +42,29 @@ readonly class CompanyController
     )]
     public function __invoke(Request $request): JsonResponse
     {
-        $users = $this->userProxy->getUsers();
+        $cacheKey = "public_companies";
 
-        $usersById = [];
-        foreach ($users as $user) {
-            $usersById[$user['id']] = $user;
-        }
+        $result = $this->cache->get($cacheKey, function (ItemInterface $item) {
+            $item->tag(['companies', 'public_companies']);
+            $item->expiresAfter(20);
+            $users = $this->userProxy->getUsers();
 
-        $companies = $this->companyRepository->findAll();
+            $usersById = [];
+            foreach ($users as $user) {
+                $usersById[$user['id']] = $user;
+            }
 
-        $response = [];
-        foreach ($companies as $key => $company){
-            $response[$key] = $company->toArray();
-            $response[$key]['user'] = $usersById[$company->getUser()->toString()] ?? null;
-        }
+            $companies = $this->companyRepository->findAll();
 
-        /** @var array<string, string|array<string, string>> $output */
-        $output = JSON::decode(
-            $this->serializer->serialize(
-                $response,
-                'json',
-                [
-                    'groups' => 'Company',
-                ]
-            ),
-            true,
-        );
+            $response = [];
+            foreach ($companies as $key => $company){
+                $response[$key] = $company->toArray();
+                $response[$key]['user'] = $usersById[$company->getUser()->toString()] ?? null;
+            }
 
-        return new JsonResponse($output);
+            return $response;
+        });
+
+        return new JsonResponse($result);
     }
 }
--- src/Job/Transport/Controller/Frontend/IndexController.php
+++ src/Job/Transport/Controller/Frontend/IndexController.php
@@ -4,16 +4,17 @@ declare(strict_types=1);
 
 namespace App\Job\Transport\Controller\Frontend;
 
-use App\General\Domain\Utils\JSON;
 use App\Job\Application\ApiProxy\UserProxy;
 use App\Job\Infrastructure\Repository\JobRepository;
 use JsonException;
 use OpenApi\Attributes as OA;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
@@ -25,9 +26,9 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 readonly class IndexController
 {
     public function __construct(
-        private SerializerInterface $serializer,
         private JobRepository       $jobRepository,
-        private UserProxy           $userProxy
+        private UserProxy           $userProxy,
+        private TagAwareCacheInterface $cache
     ) {
     }
 
@@ -38,6 +39,7 @@ readonly class IndexController
      * @throws DecodingExceptionInterface
      * @throws ClientExceptionInterface
      * @throws JsonException
+     * @throws InvalidArgumentException
      */
     #[Route(path: '/platform/job', methods: [Request::METHOD_GET])]
     public function __invoke(Request $request): JsonResponse
@@ -45,83 +47,78 @@ readonly class IndexController
         $page = max((int)$request->query->get('page', 1), 1);
         $limit = max((int)$request->query->get('limit', 20), 1);
         $offset = ($page - 1) * $limit;
+        $cacheKey = "public_jobs_page_{$page}_limit_{$limit}";
 
-        // Sinon, fallback sur DQL classique
-        $users = $this->userProxy->getUsers();
-        $usersById = [];
-        foreach ($users as $user) {
-            $usersById[$user['id']] = $user;
-        }
+        $result = $this->cache->get($cacheKey, function (ItemInterface $item) use ($limit, $offset, $page, $request) {
+            $item->tag(['jobs', 'public_jobs']);
+            $item->expiresAfter(20);
+            $users = $this->userProxy->getUsers();
+            $usersById = [];
+            foreach ($users as $user) {
+                $usersById[$user['id']] = $user;
+            }
 
-        $qb = $this->jobRepository->createQueryBuilder('j');
+            $qb = $this->jobRepository->createQueryBuilder('j');
 
-        if ($title = $request->query->get('title')) {
-            $qb->andWhere('j.title LIKE :title')
-                ->orWhere('j.description LIKE :title')
-                ->setParameter('title', "%$title%");
-        }
+            if ($title = $request->query->get('title')) {
+                $qb->andWhere('j.title LIKE :title')
+                    ->orWhere('j.description LIKE :title')
+                    ->setParameter('title', "%$title%");
+            }
 
-        if ($companyName = $request->query->get('company')) {
-            $qb->join('j.company', 'c')
-                ->andWhere('c.name LIKE :companyName')
-                ->setParameter('companyName', "%$companyName%");
-        }
+            if ($companyName = $request->query->get('company')) {
+                $qb->join('j.company', 'c')
+                    ->andWhere('c.name LIKE :companyName')
+                    ->setParameter('companyName', "%$companyName%");
+            }
 
-        if ($location = $request->query->get('location')) {
-            $qb->join('j.company', 'c2')
-                ->andWhere('j.workLocation LIKE :location')
-                ->orWhere('c2.location LIKE :location')
-                ->setParameter('location', "%$location%");
-        }
+            if ($location = $request->query->get('location')) {
+                $qb->join('j.company', 'c2')
+                    ->andWhere('j.workLocation LIKE :location')
+                    ->orWhere('c2.location LIKE :location')
+                    ->setParameter('location', "%$location%");
+            }
 
-        if ($experience = $request->query->get('experience')) {
-            $qb->andWhere('j.experience = :experience')
-                ->setParameter('experience', $experience);
-        }
+            if ($experience = $request->query->get('experience')) {
+                $qb->andWhere('j.experience = :experience')
+                    ->setParameter('experience', $experience);
+            }
 
-        $works = $request->query->all('works');
-        if (!empty($works)) {
-            $qb->andWhere('j.workType IN (:works)')
-                ->setParameter('works', $works);
-        }
+            $works = $request->query->all('works');
+            if (!empty($works)) {
+                $qb->andWhere('j.workType IN (:works)')
+                    ->setParameter('works', $works);
+            }
 
-        $contracts = $request->query->all('contracts');
-        if (!empty($contracts)) {
-            $qb->andWhere('j.contractType IN (:contracts)')
-                ->setParameter('contracts', $contracts);
-        }
+            $contracts = $request->query->all('contracts');
+            if (!empty($contracts)) {
+                $qb->andWhere('j.contractType IN (:contracts)')
+                    ->setParameter('contracts', $contracts);
+            }
 
-        $skills = $request->query->all('skills');
-        if (!empty($skills)) {
-            $qb->andWhere('j.requiredSkills IN (:skills)')
-                ->setParameter('skills', $skills);
-        }
+            $skills = $request->query->all('skills');
+            if (!empty($skills)) {
+                $qb->andWhere('j.requiredSkills IN (:skills)')
+                    ->setParameter('skills', $skills);
+            }
 
-        $qb->orderBy('j.createdAt', 'DESC');
-        $qb->setFirstResult($offset)->setMaxResults($limit);
+            $qb->orderBy('j.createdAt', 'DESC');
+            $qb->setFirstResult($offset)->setMaxResults($limit);
 
-        $jobs = $qb->getQuery()->getResult();
+            $jobs = $qb->getQuery()->getResult();
 
-        $response = [];
-        foreach ($jobs as $key => $job) {
-            $applied = null;
-            $response[$key] = $job->toArray();
-            $response[$key]['owner'] = null;
-            $response[$key]['applied'] = $applied !== null;
-            $response[$key]['user'] = $usersById[$job->getUser()->toString()] ?? null;
-        }
+            $data = [];
 
-        $output = JSON::decode(
-            $this->serializer->serialize($response, 'json', [
-                'groups' => 'Job',
-            ]),
-            true
-        );
-        return new JsonResponse([
-            'data' => $output,
-            'page' => $page,
-            'limit' => $limit,
-            'count' => count($this->jobRepository->findAll()),
-        ]);
+            foreach ($jobs as $key => $job) {
+                $applied = null;
+                $data[$key] = $job->toArray();
+                $data[$key]['owner'] = null;
+                $data[$key]['applied'] = $applied !== null;
+                $data[$key]['user'] = $usersById[$job->getUser()->toString()] ?? null;
+            }
+            return ['data' => $data, 'page' => $page, 'limit' => $limit, 'count' => count($this->jobRepository->findAll())];
+        });
+
+        return new JsonResponse($result);
     }
 }
